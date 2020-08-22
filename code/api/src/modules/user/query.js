e6c7b7662e8fbbe313232c719ad3c490676798b2
Derek Borski <derek.borski@gmail.com>
2020-08-22 13:11:48 -0600

    Query annotations

--- code/api/src/modules/user/query.js
+++ code/api/src/modules/user/query.js
@@ -47,3 +47,7 @@ export const userGenders = {
   type: new GraphQLList(UserGenderType),
   resolve: getGenders
 }
+
+// ANNOTATION: A general outline of the query file is that it is a collection of all the queries that just get (and not change) data
+// The actual code for the database queries live in the resolvers.js file, but this file appears to act as a middleman between our frontend and those queries
+// and directs the requests to the proper function in resolvers. Maybe could look at this file like a controller in rails
