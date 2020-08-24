50a25fb84ad8380d14aeed71f9094905af1a7ae5
Danny Ramos <danramos21@gmail.com>
2020-08-23 18:38:37 -0600

    product model added annotations

--- code/api/src/modules/product/model.js
+++ code/api/src/modules/product/model.js
@@ -26,3 +26,9 @@ module.exports = function(sequelize, DataTypes) {
 
 // ANNOTATION: This is our product model. Its telling us that it has 6 datatypes. These datatypes are name, slug, description,
 // type, gender, image.
+
+
+// NOTE:jg - For the survey we would probably want to add a column that gives the article of clothing a style classification. (Will need to figure out how to do migrations)
+// NOTE:jg - gothFarmer = 1, preppy = 2 etc. 
+// NOTE:jg - will this need a new column that assocates a product with a crate? 
+// NOTE:jg - Maybe a belongs to assocation?
