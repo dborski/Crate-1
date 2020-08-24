eefd37291d29b85256f0607cef4b76ecf26dad94
Danny Ramos <danramos21@gmail.com>
2020-08-23 18:37:48 -0600

    added to crate model annotation

--- code/api/src/modules/crate/model.js
+++ code/api/src/modules/crate/model.js
@@ -21,3 +21,9 @@ module.exports = function(sequelize, DataTypes) {
 // Lines 13 - 15 are telling us that Crate has many Subscriptions
 
 // NOTE Might need to create a migration for a one to many relationship with Products
+
+// ANNOTATION:jg - sets the relationship to the subscription table. Allows it to have a many to many relationship with user. 
+// question? Are the multiple styles of crates? that would make sense with a many to many relationship. 
+// NOTE:jg - Will need a has many products assocation to. 
+// NOTE:jg - each crate will be comprised of many product objects with a certain style value
+// NOTE:jg - do we set the style value to the subscription/crate or user? Pros and cons to both.
