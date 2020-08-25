2e11a6abba37bab6e5b182c1bea9864138890c5a
Derek Borski <derek.borski@gmail.com>
2020-08-25 17:50:39 -0600

    Add userUpdate mutation

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -3,7 +3,7 @@ import { GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
 import { UserType } from './types'
-import { create, remove } from './resolvers'
+import { create, remove, update } from './resolvers'
 
 // Create
 export const userSignup = {
@@ -22,9 +22,46 @@ export const userSignup = {
     password: {
       name: 'password',
       type: GraphQLString
+    },
+
+    stylePreference: {
+      name: 'stylePreference',
+      type: GraphQLString
     }
   },
   resolve: create
+}
+
+// Update
+export const userUpdate = {
+  type: UserType,
+  args: {
+    id: {
+      name: 'id',
+      type: GraphQLInt
+    },
+
+    name: {
+      name: 'name',
+      type: GraphQLString
+    },
+
+    email: {
+      name: 'email',
+      type: GraphQLString
+    },
+
+    password: {
+      name: 'password',
+      type: GraphQLString
+    },
+
+    stylePreference: {
+      name: 'stylePreference',
+      type: GraphQLString
+    }
+  },
+  resolve: update
 }
 
 // Remove
